adbf9be4521f33b8b33c7eba3360dc41f6da81d1
Bell <admin@stretch.com>
2023-04-20 19:08:01 +0400

    timezone update system

--- example/availability.py
+++ example/availability.py
@@ -1,6 +1,7 @@
 import os
 import time
-
+from pprint import pp
+import logging
 from stretch import Stretch
 
 client_id = os.getenv("MY_ACCOUNT_CLIENT_ID", None)
@@ -10,20 +11,32 @@ client_id = "2f9445b3-5266-45cd-8a85-d5c3fff69781"
 base_url = "http://localhost:8000"
 
 
+logging.basicConfig(level=logging.INFO)
+
+
+def coverage_test(stretch):
+    resp = stretch.coach.post_availability(
+        title="Availability slot", startSlot="13:30", endSlot="17:30", slotType="monday"
+    )
+    pp(resp)
+
+
 def main():
     print("Start availability example")
     stretch = Stretch(base_url=base_url, client_id=client_id, profiling=True)
 
-    #login = stretch.auth.login("coach0", "123456")
+    # login = stretch.auth.login("coach0", "123456")
     login = stretch.auth.login("bell", "123456")
 
     if login:
         resp = stretch.auth.get_user()
-        print(resp)
+        pp(resp)
         resp = stretch.coach.get_availability()
-        print(resp)
-        # resp = stretch.storage.post_image(url, "title image")
-        # print(resp)
+        pp(resp)
+        resp = stretch.coach.post_availability(
+            title="Availability slot", slot_start="13:00", slot_end="17:30", slot_type="monday"
+        )
+        pp(resp)
 
     return
 
